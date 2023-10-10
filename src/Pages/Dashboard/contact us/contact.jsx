6843a712f0e67a686c92544e43ad6e94f4575976
itheakmal <94047823+itheakmal@users.noreply.github.com>
2023-10-10 19:14:00 +0500

    social media links

--- src/Pages/Dashboard/contact us/contact.jsx
+++ src/Pages/Dashboard/contact us/contact.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './contact.css'
 import atricentLogo from '../../../assets/images/Layer 1.png'
 import facebook from '../../../assets/images/Facebook.png'
-import google from '../../../assets/images/Google.png'
-import twitter from '../../../assets/images/Twitter.png'
+import youtube from '../../../assets/images/youtube-logo-circle-red-social-media-logo_197792-4982.avif'
+import tiktok from '../../../assets/images/tiktok-icon-free-png.webp'
+import instgram from '../../../assets/images/purple-gradiend-social-media-logo_197792-1883.avif'
 import address from '../../../assets/images/address.png'
 import shape from '../../../assets/images/Shape.png'
 import telephone from '../../../assets/images/telephone.png'
@@ -18,46 +19,52 @@ import {
 const Contact = () => {
   return (
     <Container className='contact-us-container'>
-    <Row>
-      <Col xs="12" md="4">
-        <div className='contact-first-col'>
-          {/* Logo */}
-          <img src={atricentLogo} alt="Atricent Logo" />
+      <Row>
+        <Col xs="12" md="4">
+          <div className='contact-first-col'>
+            {/* Logo */}
+            <img src={atricentLogo} alt="Atricent Logo" />
 
-          <p>Copyright © 2020 Atricent</p>
+            <p>Copyright © 2020 Atricent</p>
 
-          <div className='social-media-logos'>
-            <img src={facebook} alt="Logo 1" className='fb-img'/>
-            <img src={google} alt="Logo 2" className='fb-img'/>
-            <img src={twitter} alt="Logo 3" className='twitter-img'/>
+            <div className='social-media-logos'>
+              <a href="https://www.instagram.com/atricenthq/" target="_blank" rel="noopener noreferrer">
+                <img src={instgram} alt="Logo 1" className='fb-img' />
+              </a>
+              <a href="https://www.youtube.com/channel/UCQUP6oCVUA-wDDPiI0kegvg" target="_blank" rel="noopener noreferrer">
+                <img src={youtube} alt="Logo 2" className='fb-img' />
+              </a>
+              <a href="https://www.tiktok.com/@atricent" target="_blank" rel="noopener noreferrer">
+                <img src={tiktok} alt="Logo 3" className='twitter-img' />
+              </a>
+            </div>
           </div>
-        </div>
-      </Col>
+        </Col>
 
-      <Col xs="12" md="4">
-        <div>
-          <h2>About Atricent</h2>
-          <nav className='About-Atricent'>
-            <ul>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Outfit</li>
-              <li>Wardrobe</li>
-            </ul>
-          </nav>
-        </div>
-      </Col>
+        <Col xs="12" md="4">
+          <div>
+            <h2>About Atricent</h2>
+            <nav className='About-Atricent'>
+              <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Outfit</li>
+                <li>Wardrobe</li>
+              </ul>
+            </nav>
+          </div>
+        </Col>
 
-      <Col xs="12" md="4">
-        <div>
-          <h2>Contact Us</h2>
-          <p className='address-parent'><span className='address-parent-img'><img src={address} alt="" /></span>Address: 4th Floor, Business Centre, Gulberg III, Lahore</p>
-          <p className='email-parent'><span className='email-parent-img'><img src={shape} alt="" /></span>info@Atricent.com</p>
-          <p className='phone-parent'><span className='phone-parent-img'><img src={telephone} alt="" /></span>XXX XXX XXX</p>
-        </div>
-      </Col>
-    </Row>
-  </Container>
+        <Col xs="12" md="4">
+          <div>
+            <h2>Contact Us</h2>
+            <p className='address-parent'><span className='address-parent-img'><img src={address} alt="" /></span>Address: 4th Floor, Business Centre, Gulberg III, Lahore</p>
+            <p className='email-parent'><span className='email-parent-img'><img src={shape} alt="" /></span>info@Atricent.com</p>
+            <p className='phone-parent'><span className='phone-parent-img'><img src={telephone} alt="" /></span>XXX XXX XXX</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- src/Pages/Dashboard/mobilesection/mobile.jsx
+++ src/Pages/Dashboard/mobilesection/mobile.jsx
@@ -10,31 +10,34 @@ import qrcode from '../../../assets/images/qrcode.png'
 
 const Mobile = () => {
   return (
-   <Container className='modile-container'>
-        <div className='mobile-images'>
-                <img src={mobileImage} alt="" />
-        </div>
-        <div className='main-mobile-div'>
+    <Container className='modile-container'>
+      <div className='mobile-images'>
+        <img src={mobileImage} alt="" />
+      </div>
+      <div className='main-mobile-div'>
         <div className='mobile-text-div'>
           <h1>You can<br /> download the<br /> apps.</h1>
           <p>Lorem ipsum dolor sit amet consectetur. Maecenas<br /> vivamus urna eu sed eget imperdiet. Quisque viverra<br /> leo quis gravida mattis donec malesuada.</p>
         </div>
-          <div className='image-apps-div'>
+        <div className='image-apps-div'>
+          <a href="https://apps.apple.com/gb/app/atricent-ai-fashion-platform/id6448105847" target="_blank" rel="noopener noreferrer">
             <div className='apple-image-div'>
-
-            <img src={AppleImg} alt="" />
+              <img src={AppleImg} alt="" />
             </div>
+          </a>
+
+          <a href="https://play.google.com/store/apps/details?id=com.atricent&hl=en_US" target="_blank" rel="noopener noreferrer">
             <div className='playstore-image-div'>
-            <img src={PlaystoreImg} alt="" />
-
+              <img src={PlaystoreImg} alt="" />
             </div>
-          </div>
-          <div className='qrcode'>
-            <img src={qrcode} alt="" />
-          </div>
+          </a>
         </div>
-        
-   </Container>
+        <div className='qrcode'>
+          <img src={qrcode} alt="" />
+        </div>
+      </div>
+
+    </Container>
   )
 }
 
